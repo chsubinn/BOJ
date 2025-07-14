a1110d181900825603f79fbe453254b74ef9dcb4
Subin Cho <chosubinn@ewhain.net>
2025-07-14 14:49:56 +0900

    2565번 전깃줄 수정

--- src/DP/BOJ_2565/Main.java
+++ src/DP/BOJ_2565/Main.java
@@ -14,45 +14,28 @@ public class Main {
 
         int n = Integer.parseInt(st.nextToken());
         int [][] graph = new int [n][2];
-        int [] dp = new int [501];
+        int [] dp = new int [n];
 
         for (int i=0; i<n; i++){
             st = new StringTokenizer(br.readLine(), " ");
             for (int j=0; j<2; j++){
                 graph[i][j] = Integer.parseInt(st.nextToken());
             }
+            dp[i] = 1;
         }
+        Arrays.sort(graph, Comparator.comparingInt(a -> a[0]));
 
-        int max_cross = 0;
-        int cnt = 0;
-        while (true){
-            for (int i=0; i<n; i++){ // 기준
-                for (int j=0; j<n; j++){
-                    if (graph[i][0] < graph[j][0] && graph[i][1]>graph[j][1]){
-                        dp[graph[i][0]] ++;
-                        dp[graph[i][1]] ++;
-                    }
-                    max_cross = Math.max(max_cross, dp[graph[i][0]]);
+        int max_val = 0;
+        for (int i=0; i<n; i++){
+            for (int j=0; j<i; j++){
+                if (graph[j][1] < graph[i][1]){
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
-            for (int i=1; i<10; i++){
-                System.out.print(dp[i]+ " ");
-            }
-            System.out.println();
-
-            if (max_cross == 0){
-                break;
-            }
-            for (int i=0; i<501; i++){
-                if (max_cross == dp[i]){
-                    cnt ++;
-                    dp[i] = -1;
-
-                }
-            }
-            max_cross=0;
+            max_val = Math.max(max_val, dp[i]);
         }
-        bw.write(String.valueOf(cnt/2));
+
+        bw.write(String.valueOf(n-max_val));
         bw.flush();
         bw.close();
     }
