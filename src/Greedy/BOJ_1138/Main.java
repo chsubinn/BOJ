1c88362bda8b76d0d50bff95c6d8449b5d1c78e1
Subin Cho <chosubinn@ewhain.net>
2025-07-23 00:26:31 +0900

    1138번 한 줄로 서기 수정

--- src/Greedy/BOJ_1138/Main.java
+++ src/Greedy/BOJ_1138/Main.java
@@ -5,6 +5,7 @@ import java.util.*;
 
 // https://www.acmicpc.net/problem/1138
 // 1138번 한 줄로 서기
+// 수정 후: 수정 가능한 리스트 ArrayList로 삽입할 생각조차 못했음.,,ㅎㅎㅎ
 
 public class Main {
     public static void main (String [] args) throws IOException {
@@ -13,30 +14,26 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken());
-        int [] graph = new int [n+1];
-        int [] answer = new int [n];
+        int [] graph = new int [n];
 
         st = new StringTokenizer(br.readLine(), " ");
-        for (int i=1; i<n+1; i++){
+        for (int i=0; i<n; i++){
             graph[i] = Integer.parseInt(st.nextToken());
         }
 
         // logic
-        for (int i=1; i<n+1; i++){
-            if (answer[graph[i]] == 0){
-                answer[graph[i]] = i;
-            }
-            else if (answer[graph[i]] < i){
-                answer[graph[i]+i-1] = i;
-            }
+        List<Integer> line = new ArrayList<>();
+        for (int i=n; i>0; i--){
+            line.add(graph[i-1], i); // graph[i-1]자리에 숫자 i 넣기
+            // 어차피 뒤에서부터 접근하는 인덱스 순으로 키가 클테니까,, 뒤로 밀려나는게 요구사항면에서도 맞다!!
         }
 
-        for (int i=0; i<n; i++){
-            if (i!=n-1){
-                bw.write(answer[i] + " ");
+        for (int i=0; i<line.size(); i++){
+            if (i!=line.size()-1){
+                bw.write(line.get(i) + " ");
             }
             else{
-                bw.write(String.valueOf(answer[i]));
+                bw.write(String.valueOf(line.get(i)));
             }
         }
         bw.flush();
