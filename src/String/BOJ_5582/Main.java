a4045e20f3df5acca5ed9611cb223d891157cfc1
Subin Cho <chosubinn@ewhain.net>
2025-07-29 13:54:48 +0900

    5582번 공통 부분 문자열 수정

--- src/String/BOJ_5582/Main.java
+++ src/String/BOJ_5582/Main.java
@@ -6,39 +6,36 @@ import java.util.*;
 // https://www.acmicpc.net/problem/5582
 // 5582번 공통 부분 문자열
 
+// 수정 전: 브루트포스 방식, O(n^3)의 시간복잡도이므로 최대 문자열 길이 50만 처리 가능 -> 시간초과 발생
+// 수정 후: DP로 구현하여 O(n*m)의 시간복잡도,,
+
 public class Main {
     public static void main (String args []) throws IOException {
         BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
         BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
 
+        String A = br.readLine();
+        String B = br.readLine();
+        int N = A.length();
+        int M = B.length();
+
+        int[][] dp = new int[N + 1][M + 1]; // dp[i][j] = A의 i번째 문자까지와 B의 j번쨰 문자까지 중 가장 긴 공통 부분 문자열의 길이
         int answer = 0;
-        String [] string = new String [2];
-        string[0] = br.readLine();
-        string[1] = br.readLine();
 
-        Arrays.sort(string, Comparator.comparingInt(String::length));
-        int N = string[0].length();
-        int M = string[1].length();
-
-        for (int i=N; i>0; i--){
-            for (int k=0; k<=N-i; k++){
-                String slice0 = string[0].substring(k, k+i);
-                for (int j=0; j<M-i; j++){
-                    String slice1 = string[1].substring(j, j+i);
-                    if (slice0.equals(slice1)){
-                        answer = Math.max(answer, i);
-                        break;
-                    }
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= M; j++) {
+                if (A.charAt(i - 1) == B.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    answer = Math.max(answer, dp[i][j]);
+                } else {
+                    dp[i][j] = 0;
                 }
-                if (answer==i) break;
             }
-            if (answer>0) break;
         }
 
         bw.write(String.valueOf(answer));
         bw.flush();
         bw.close();
-
     }
 
 }
