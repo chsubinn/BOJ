ba4e26b1630e62c5fc9d6fabb42ade09fc1e0f5f
Subin Cho <chosubinn@ewhain.net>
2025-08-05 16:29:28 +0900

    17144번 미세먼지 안녕

--- src/Simulation/BOJ_17144/Main.java
+++ src/Simulation/BOJ_17144/Main.java
@@ -2,6 +2,9 @@ package Simulation.BOJ_17144;
 
 // https://www.acmicpc.net/problem/17144
 // 17144번 미세먼지 안녕!
+// 오답노트
+// 1. visited가 아니라 temp 배열에 퍼진 먼지 모두 반영하고 마지막에 더해줘야 합니다 <- visited는 한번만 처리하게 하지만 우리는 여러번 먼제를 확산 시켜야 함
+// 2. air refresher: 일일이 값을 넘기지 않아도,,, 된다,,,,,,,,, 그리고 마지막으로 도달하는 방향(?) 먼저 넣어줘야 덮어쓰이지 않는다,,,,,,, 
 
 import java.util.*;
 import java.io.*;
@@ -58,8 +61,6 @@ public class Main {
 
         for (int i=0; i<R; i++){
             for (int j=0; j<C; j++){
-                visited[i][j] = 1;
-
                 if (graph[i][j] == -1 || graph[i][j] == 0) continue;
 
                 int movingDust = graph[i][j] / 5;
@@ -69,16 +70,20 @@ public class Main {
                     int nr = dr[k] + i;
                     int nc = dc[k] + j;
 
-                    if (nr>=0 && nr < R && nc >= 0 && nc < C && graph[nr][nc] != -1 && visited[nr][nc]!=1){
+                    if (nr>=0 && nr < R && nc >= 0 && nc < C && graph[nr][nc] != -1){
                         numOfMovingDust++;
-                        graph[nr][nc] += movingDust;
-                        visited[nr][nc] = 1;
+                        visited[nr][nc] += movingDust;
                     }
                 }
-                remainingDust -= movingDust * numOfMovingDust;
-                graph[i][j] = remainingDust;
+                visited[i][j] += graph[i][j] - movingDust * numOfMovingDust;
             }
         }
+
+        for (Pair pair : refresher){
+            visited[pair.r][pair.c] = -1;
+        }
+
+        graph = visited;
     }
 
     static private void airRefresh(){
@@ -86,42 +91,21 @@ public class Main {
         Pair upper = refresher.get(0);
         Pair downer = refresher.get(1);
 
-        int [] drForUpper = {0, 1, 0, -1};
-        int [] dcForUpper = {1, 0, -1, 0};
-        int [] drForDowner = {0, -1, 0, 1};
-        int [] dcForDowner = {1, 0, -1, 0};
-
         // for Upper
-        int prev = 0;
-        for (int d = 0; d<4; d++){
-            int ni = upper.r + drForUpper[d];
-            int nj = upper.c + dcForUpper[d];
-            while (ni >= 0 && ni < R && nj>=0 && nj<C && ni!=upper.r && nj != upper.c){
+        for (int i = upper.r - 1; i > 0; i--) graph[i][0] = graph[i - 1][0]; // 위에서 아래로
+        for (int j = 0; j < C - 1; j++) graph[0][j] = graph[0][j + 1]; // 오른쪽에서 왼쪽으로
+        for (int i = 0; i < upper.r; i++) graph[i][C - 1] = graph[i + 1][C - 1]; // 아래에서 위로
+        for (int j = C - 1; j > 1; j--) graph[upper.r][j] = graph[upper.r][j - 1]; // 
 
-                int temp = graph[ni][nj];
-                graph[ni][nj] = prev;
-                prev = temp;
+        graph[upper.r][1] = 0;  
 
-                ni += drForUpper[d];
-                nj += dcForUpper[d];
-            }
-        }
-
-        // for Downer
-        prev = 0;
-        for (int d = 0; d<4; d++){
-            int ni = downer.r + drForDowner[d];
-            int nj = downer.c + dcForDowner[d];
-            while (ni >= 0 && ni < R && nj>=0 && nj<C && ni!=downer.r && nj != downer.c){
-
-                int temp = graph[ni][nj];
-                graph[ni][nj] = prev;
-                prev = temp;
-
-                ni += drForDowner[d];
-                nj += dcForDowner[d];
-            }
-        }
+        // 아래쪽 (시계)
+        for (int i = downer.r + 1; i < R - 1; i++)  graph[i][0] = graph[i + 1][0];
+        for (int j = 0; j < C - 1; j++) graph[R - 1][j] = graph[R - 1][j + 1];
+        for (int i = R - 1; i > downer.r; i--) graph[i][C - 1] = graph[i - 1][C - 1];
+        for (int j = C - 1; j > 1; j--) graph[downer.r][j] = graph[downer.r][j - 1];
+        
+        graph[downer.r][1] = 0; 
     }
 
     static private class Pair implements Comparable<Pair>{
